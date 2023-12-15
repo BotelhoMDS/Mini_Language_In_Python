78992fc250aade0087c2c4a0f0a978203ec29882
crashertn <crashertn@hotmail.com>
2023-12-15 17:37:02 -0300

    First semantic rules

--- yacc.py
+++ yacc.py
@@ -19,34 +19,46 @@ class parser(object):
     def p_body(self,p):
         '''body : DECLARE declaration_list BEGIN statement_list END'''
 
+
     def p_declaration_list(self,p):
         '''declaration_list : declaration SEMICOLON declaration_list_aux'''
+        p[0] = p[1] + ';' + p[3]
 
     def p_declaration_list_aux(self,p):#Auxiliar pra evitar ambiguidade
         '''declaration_list_aux : declaration_list
                             | empty'''
+        p[0] = p[1]
     
     def p_declaration(self,p):
         '''declaration : type identifier_list'''
+        p[0].type = p[1].type
 
-    def p_identifier_list(self,p):
+
+    def p_identifier_list(self,p):#VERIFICAR
         '''identifier_list : ID identifier_list_aux'''  
+        p[0]=p[1]
+        #self.type = p[1].type
 
     def p_identifier_list_aux(self,p):
         '''identifier_list_aux : COMMA identifier_list
                             | empty '''  
+        if (len(p) == 3):
+            p[0] = p[2]
+        elif (len(p) == 2):
+            p[0] = p[1]
 
     def p_type(self,p):
         '''type : INTEGER 
                 | DECIMAL'''
+        p[0] = p[1]
 
     def p_statement_list(self,p): #revisar depois 
-        '''statement_list : statement SEMICOLON statement_prime
-
-                        ''' 
+        '''statement_list : statement SEMICOLON statement_prime'''
+        p[0]=[1] 
     def p_statement_prime(self,p):
         ''' statement_prime : statement_list
                             | empty'''
+        p[0] = p[1]
 
     def p_statement(self,p):
         '''statement : assign_statement
@@ -57,12 +69,15 @@ class parser(object):
                     | write_statement
                     | do_while_statement
                     '''
-        
-    def p_assign_statement(self,p):
+        p[0]=p[1]
+
+    def p_assign_statement(self,p):#Voltar pra revisar
         '''assign_statement : ID ASSIGN expression'''
+        p[0] = p[1] + p[2] + p[3]
 
     def p_if_statement(self,p):
         '''if_statement : IF condition THEN statement_list if_statement_aux'''
+
 
     def p_if_statement_aux(self,p):
         '''if_statement_aux : END
@@ -85,16 +100,24 @@ class parser(object):
 
     def p_write_statement(self,p):
         '''write_statement : WRITE LPAREN writable RPAREN'''
+        p[0]=p[3]
+
     def p_writable(self,p):
         '''writable : simple_expression
                         | literal'''
+        p[0]=p[1]
+
     def p_condition(self,p):
         '''condition : expression'''
+        if p[1] == True or p[1] == False:
+            p[0] = p[1]
+
     def p_expression(self,p):
         '''expression : simple_expression expression_aux'''
     def p_expression_aux(self,p):
         '''expression_aux : relop simple_expression
                     | empty'''
+        p[0]=p[1]
     def p_relop(self,p):
         '''relop : EQUAL 
                 | LESS 
@@ -102,49 +125,77 @@ class parser(object):
                 | GREATER 
                 | GREATEREQUAL 
                 | LESSGREATER'''
+        p[0]=p[1]
 
     def p_par_expression(self,p):
         '''par_expression : LPAREN expression RPAREN'''
-
+        p[0]=p[2]
         
     def p_simple_expression(self,p):
         '''simple_expression : term
                             | par_expression INTERROGATION simple_expression COLON simple_expression
-                            | simple_expression addop term
-                       '''
+                            | simple_expression addop term'''
+        if (len(p) == 2):#term
+            p[0]=p[1]
+        elif (len(p) == 4):
+            if p[2] == '+':#simple_expression addop term
+                p[0]= p[1] + p[3]
+            if p[2] == '-':
+                p[0]= p[1] - p[3]
+            if p[2] == 'or':
+                p[0]= p[1] or p[3]
+        elif (len(p) == 6):#par_expression INTERROGATION simple_expression COLON simple_expression
+            if isinstance(p[1]==bool):
+                if p[1] == True:
+                    p[0] = p[3]
+                else:
+                    p[0] = p[5]
+
 
     def p_addop(self,p):
         '''addop : PLUS 
                 | MINUS 
                 | OR'''
+        p[0]=p[1]
 
     def p_mulop(self,p):
         '''mulop : MULTI 
                 | DIVIDE 
                 | MOD 
                 | AND'''
+        p[0]=p[1]
 
     def p_term(self,p):
         '''term : factor_a
                 | term mulop factor_a'''
+        p[0]=p[1]
         
-    def p_factor_a(self,p):
+    def p_factor_a(self,p):#Ver depois
         '''factor_a : factor 
                 | NOT factor 
                 | MINUS factor'''
+        if (len(p) == 2):
+            p[0]=p[1]
+        elif p[1] == 'not':
+            p[0] = not p[1]
+        elif p[1] == '-':
+            p[0] = -p[1]
 
     def p_factor(self,p):
         '''factor : ID 
                 | NUMBER 
                 | par_expression'''
-        
+        p[0]=p[1]
 
     def p_literal(self,p):
         '''literal : LITERAL'''
+        p[0]=p[1]
 
     def p_empty(self,p): #Produções vazias, usado para retirar ambiguidades em certas regras como p_declaration_list
         'empty :'
         pass
-   
+
+    def p_error(self,p):
+        print(f'Syntax error at {p.value!r}')
 
     
